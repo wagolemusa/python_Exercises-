0683217c57c265d18adfc5b19d9108b89a68f2d3
kisumutalent <homiemusa@gmail.com>
2018-02-26 12:51:06 +0300

    radius of circle to compute the area

---
+++ circles.py
@@ -0,0 +1,6 @@
+#program which accepts the radius of a circle from the user and compute the area.
+
+from math import pi
+r = float(input("input your number"))
+
+print("The are of the circle with radius" + str() + " ls:" + str(pi * r**2))
